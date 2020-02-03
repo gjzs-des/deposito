3aad2e6bb6c0b1c5539f987e168f49830c5763ad
Gabriela Zavala <Propietario@pc-1559579848>
2020-02-03 10:55:33 +0100

    pruebas

---
+++ DepositoCombustible/src/combustible/lleno.java
@@ -0,0 +1,109 @@
+package combustible;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class lleno {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testDepositoCombustible() {
+		fail("10");
+	}
+
+	@Test
+	public void testGetDepositoNivel() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		fail("40");
+	}
+
+	@Test
+	public void testEstaVacio() {
+		fail("0");
+	}
+
+	@Test
+	public void testEstaLleno() {
+		fail("40");
+	}
+
+	@Test
+	public void testFill() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testObject() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testClone() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testToString() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testWait() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("No implementado aun");
+	}
+
+}
